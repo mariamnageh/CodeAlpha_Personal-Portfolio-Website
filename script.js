9cfafdf70633ef4af87f6d300f462ce73716dee0
mariamnageh <mariamnageh27@gmail.com>
2024-04-14 16:05:21 +0200

    Adding CV

--- script.js
+++ script.js
@@ -20,3 +20,83 @@ pageTurnBtn.forEach((el, index) => {
         }
     }
 })
+
+// contact me button
+const pages = document.querySelectorAll('.book-page.page-right');
+const contactMeBtn = document.querySelector('.btn.contact-me');
+
+contactMeBtn.onclick = () => { 
+    pages.forEach((page, index) => { 
+        setTimeout(() => { 
+            page.classList.add('turn');
+            setTimeout(() => { 
+                page.style.zIndex = 20 + index;
+            }, 500)
+        }, (index + 1) * 200 +100)
+    })
+}
+
+let totalPages = pages.length;
+let pageNumber = 0;
+
+function reverseIndex() { 
+    pageNumber--;
+    if (pageNumber < 0) { 
+        pageNumber = totalPages - 1;
+    }
+}
+
+const backProfileBtn = document.querySelector('.back-profile');
+backProfileBtn.onclick = () => { 
+    pages.forEach((_, index) => { 
+        setTimeout(() => { 
+            reverseIndex();
+            pages[pageNumber].classList.remove('turn');  
+           
+            setTimeout(() => {
+                   reverseIndex();
+                pages[pageNumber].style.zIndex = 10 + index;
+            }, 500)
+            
+            }, (index + 1) *200 + 100)
+        })
+}
+
+
+//opening animation
+
+const coverRight = document.querySelector('.cover.cover-right');
+const pageLeft= document.querySelector('.book-page.page-left');
+
+
+setTimeout(() => { 
+    coverRight.classList.add('turn');
+}, 2100)
+
+
+setTimeout(() => { 
+    coverRight.style.zIndex = -1;
+}, 2800)
+
+
+
+setTimeout(() => { 
+    pageLeft.style.zIndex = 20;
+}, 3200)
+
+
+
+
+
+pagespages.forEach((_, index) => { 
+        setTimeout(() => { 
+            reverseIndex();
+            pages[pageNumber].classList.remove('turn');  
+           
+            setTimeout(() => {
+                   reverseIndex();
+                pages[pageNumber].style.zIndex = 10 + index;
+            }, 500)
+            
+            }, (index + 1) *200 + 100)
+        })
